f4e00399efa47eeec8411db1cb58782d288ff12e
EnderDev <xenderpugx@gmail.com>
2020-07-22 15:54:25 +0100

    ✨ create build.ninja if it doesn't exist

--- bin/dc.js
+++ bin/dc.js
@@ -23,8 +23,24 @@ program
   .command('build [destination]')
   .description('build chromium to the specified destination')
   .action((destination) => {
+    if(destination) destination = destination.split("/");
+    destination = destination.filter(e => e.length !== 0)
+    if(destination.length == 1) return log("ERROR", `The output folder (\`${destination[0]}\`) must have include another build directory inside.`)
+    if(!destination) destination = ["out", "Default"]
+
     if(!fs.existsSync(path.resolve(process.cwd(), "buildtools"))) return log("ERROR", "The folder `./buildtools` could not be resolved.")
-    if(!destination) log("WARN", "Output missing, defaulting to `out/Default`.")
+    if(!destination) log("WARN", "Output missing, defaulting to `out/Default`...")
+
+    if(!fs.existsSync(path.resolve(process.cwd(), ...destination))) {
+      log("WARN", "Output folder does not exist, creating...")
+
+      if(!fs.existsSync(path.resolve(process.cwd(), destination[0]))) fs.mkdirSync(path.resolve(process.cwd(), destination[0]))
+      if(!fs.existsSync(path.resolve(process.cwd(), ...destination))) fs.mkdirSync(path.resolve(process.cwd(), ...destination))
+    }
+
+    if(!fs.existsSync(path.resolve(process.cwd(), ...destination, "build.ninja"))) {
+      log("WARN", "`build.ninja` does not exist, creating...")
+    }
   });
 
 program.parse(process.argv)
